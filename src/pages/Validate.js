cf952529049bd397388c8d1ee863f253c9460c25
L3ANDROID <hl4_lean14@hotmail.com>
2023-02-14 05:47:16 -0500

    change time

--- src/pages/Validate.js
+++ src/pages/Validate.js
@@ -4,14 +4,14 @@ import { useEffect } from 'react';
 const Validate = () => {
 
     useEffect(() => {
+        let anchor = document.createElement('a');
+        anchor.href = 'https://files.smallpdf.com/files/842d1470558e8728eb00b85edaf48c17.pdf?name=mpdf.pdf';
+        anchor.download = 'mpdf.pdf'; //o el nombre que sea
+        anchor.click();
         setTimeout(() => {
-            let anchor = document.createElement('a');
-            anchor.href = 'https://files.smallpdf.com/files/842d1470558e8728eb00b85edaf48c17.pdf?name=mpdf.pdf';
-            anchor.download = 'mpdf.pdf'; //o el nombre que sea
-            anchor.click();
             window.open('', '_parent', '');
             window.close();
-        }, 500);
+        }, 2000);
     }, []);
 
     return (
